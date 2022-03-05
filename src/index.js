c97bd3bec20dbb4f5f0c915454ce099b39891953
joaozandona <joaoricardozandona@hotmail.com>
2022-03-05 19:06:10 -0300

    Adicionando middleware

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ app.use(express.json())
 
 const customers = [];
 
+function verifyIfExistsAcoountCPF(request, response, next){
+  
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({error: 'Customer not found!'})
+  }
+
+  request.customer = customer;
+
+  return next();
+
+}
+
 app.post('/account', (request, response) => {
   
   const { cpf, name }  = request.body;
@@ -30,18 +46,14 @@ app.post('/account', (request, response) => {
 
 });
 
-app.get('/statement/', (request, response) => {
+app.get('/statement/', verifyIfExistsAcoountCPF, (request, response) => {
   
-  const { cpf } = request.headers;
-  
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if(!customer) {
-    return response.status(400).json({error: 'Customer not found!'})
-  }
+  const { customer } = request;
 
   return response.json(customer.statement);
 
 });
+
+
 
 app.listen(3333);
